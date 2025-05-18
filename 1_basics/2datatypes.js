5846c5d14e6457a0fc079d04db762dbf610c1e3e
Anupam Sharma <ansh151923@gmail.com>
2025-05-18 13:58:35 +0530

    adding file

---
+++ 1_basics/2datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version
+
+// alert (3 + 4) // we are using nodejs, not browser
+
+console.log(3 + 4); // 7
+console.log("Anupam") // Anupam
+// semicolon or without semicolon it will work
+
+// datatypes - 
+let name = "anupam"
+let age = 22
+let isLoggedIn = false
+// let state = null;
+let state;
+
+// number => 2 to power 53
+// bigint = for large number values/data
+// string = "" or '' // but prefer to use ""
+// boolean => true/false
+// null =? standalone value
+// undefined =>  
+// symbol => unique
+
+
+// object
+console.log(typeof "anupam") // string
+console.log(typeof 40) // number
+console.log(typeof "4") // string
+console.log(undefined); // undefined
+console.log(typeof null); // object
