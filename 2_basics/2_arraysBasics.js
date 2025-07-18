19fd6c16c04ae228147580cfa8e769804203d1b3
Anupam Sharma <ansh151923@gmail.com>
2025-07-18 17:50:32 +0530

    updated

---
+++ 2_basics/2_arraysBasics.js
@@ -0,0 +1,4 @@
+const marvel_heroes = ["thor", "ironman", "hulk", "spiderman", "captain america", "black widow", "black panther", "doctor strange", "antman", "captain marvel"];
+
+const dc_heroes = ["superman", "batman", "wonder woman", "flash", "aquaman", "green lantern", "cyborg", "green arrow", "shazam", "martian manhunter"];
+
