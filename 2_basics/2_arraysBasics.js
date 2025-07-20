f742c78793d1dec43653640878fa23b253848a51
Anupam Sharma <ansh151923@gmail.com>
2025-07-20 20:48:20 +0530

    updated

--- 2_basics/2_arraysBasics.js
+++ 2_basics/2_arraysBasics.js
@@ -1,4 +1,41 @@
-const marvel_heroes = ["thor", "ironman", "hulk", "spiderman", "captain america", "black widow", "black panther", "doctor strange", "antman", "captain marvel"];
+const marvel_heroes = ["thor", "ironman", "hulk", "spiderman", "captain america"];
 
-const dc_heroes = ["superman", "batman", "wonder woman", "flash", "aquaman", "green lantern", "cyborg", "green arrow", "shazam", "martian manhunter"];
+const dc_heroes = ["superman", "batman", "wonder woman", "flash", "martian manhunter"];
 
+// console.log(marvel_heroes);
+// console.log(dc_heroes);
+
+marvel_heroes.push(dc_heroes)
+/*
+[
+  'thor',
+  'ironman',
+  'hulk',
+  'spiderman',
+  'captain america',
+  [
+    'superman',
+    'batman',
+    'wonder woman',
+    'flash',
+    'martian manhunter'
+  ]
+]
+  */
+console.log(marvel_heroes) // output up
+
+marvel_heroes.concat(dc_heroes)
+/*[
+  'thor',
+  'ironman',
+  'hulk',
+  'spiderman',
+  'captain america',
+  [
+    'superman',
+    'batman',
+    'wonder woman',
+    'flash',
+    'martian manhunter'
+  ]
+]* */
